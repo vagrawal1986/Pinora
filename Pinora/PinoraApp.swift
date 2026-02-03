731c6d37ca879596fbc5df6fd08094f3954d4881
Vivek Agrawal <jadu.vivek@gmail.com>
2026-02-02 20:59:21 -0600

    Initial Commit

---
+++ Pinora/PinoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinoraApp.swift
+//  Pinora
+//
+//  Created by Vivek Agrawal on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PinoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
